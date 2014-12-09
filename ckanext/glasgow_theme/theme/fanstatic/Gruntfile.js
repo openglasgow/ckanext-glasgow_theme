031e546c04032ea7160617b75be4c4711fe3e274
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2014-12-09 15:02:23 +0000

    initial work.  version of header, footer, less files set up

---
+++ ckanext/glasgow_theme/theme/fanstatic/Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+
+	// register all that available tasks
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+	// initial optipns
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['less/**/*.less'],  
+                tasks: ['buildcss']
+            }
+        },
+
+        less: {
+          development: {
+            options: {
+              paths: ["less"],
+              sourceMap: true
+            },
+            files: {
+              "glasgow_main.css": "less/glasgow_main.less"
+            }
+          }
+        }
+		
+    }) // end grunt.initCofig
+
+
+    grunt.registerTask('buildcss', ['less']);
+
+
+}
