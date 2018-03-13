73959107896e2b682cb0e72b323510228708adbe
devonwalshe <devon.walshe@gmail.com>
2018-03-13 13:32:22 +0000

    most changes from SS document included in theme plugin

--- ckanext/glasgow_theme/plugin.py
+++ ckanext/glasgow_theme/plugin.py
@@ -1,12 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-
+from ckanext.glasgow_theme.model.license import *
 
 class Glasgow_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
